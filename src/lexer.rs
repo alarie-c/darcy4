e561080f7e0f068910193359bf16c4b0f3987e0a
alarie <150884931+alarie-c@users.noreply.github.com>
2024-06-30 22:48:04 +0000

    Fixed up the lexer, added a couple more tokens

--- src/lexer.rs
+++ src/lexer.rs
@@ -2,20 +2,22 @@ use std::iter::Peekable;
 
 use crate::token::Token;
 
+// takes a stream of chars and creates tokenized output
 pub struct Lexer<Iter: Iterator<Item = char>> {
     pub stream: Peekable<Iter>,
     pub output: Vec<Token>,
     cursor: usize,
+    offset: usize,
     ch: char,
 }
 
 impl<Iter: Iterator<Item = char>> Lexer<Iter> {
-
     // creates a new lexer and advances to the first character
     pub fn new(stream: Peekable<Iter>) -> Option<Self> {
         let mut lexer = Self {
             stream,
             output: Vec::<Token>::new(),
+            offset: 0,
             cursor: 0,
             ch: '.',
         };
@@ -28,15 +30,23 @@ impl<Iter: Iterator<Item = char>> Lexer<Iter> {
         }
     }
 
+    // scans each char and tokenizes it
     pub fn scan(&mut self) {
         loop {
             self.tokenize();
-            if self.check() { continue; } else { break; }
+            if self.check() {
+                continue;
+            } else {
+                break;
+            }
         }
 
-        self.push(Token::Eof { offset: self.cursor });
+        self.push(Token::Eof {
+            offset: self.cursor,
+        });
     }
 
+    // advances the iterator and increments cursor and char values
     fn next(&mut self) -> Option<char> {
         if let Some(character) = self.stream.next() {
             self.ch = character;
@@ -47,6 +57,7 @@ impl<Iter: Iterator<Item = char>> Lexer<Iter> {
         }
     }
 
+    // advances the iterator and returns if it was successful or not
     fn check(&mut self) -> bool {
         if let Some(_) = self.next() {
             true
@@ -55,13 +66,15 @@ impl<Iter: Iterator<Item = char>> Lexer<Iter> {
         }
     }
 
+    // pushes a given token to the output
     fn push(&mut self, token: Token) {
         self.output.push(token)
     }
 
+    // pushes chars to a buffer until a symbol is reached
     fn take_alphanum(&mut self) -> String {
         let mut buffer = String::from(self.ch);
-        
+
         loop {
             if let Some(character) = self.next() {
                 if character.is_alphanumeric() || character == '_' {
@@ -78,9 +91,10 @@ impl<Iter: Iterator<Item = char>> Lexer<Iter> {
         buffer
     }
 
+    // pushes numbers to a buffer until a non-number is found
     fn take_number(&mut self) -> String {
         let mut buffer = String::from(self.ch);
-        
+
         loop {
             if let Some(character) = self.next() {
                 if character.is_numeric() {
@@ -99,9 +113,10 @@ impl<Iter: Iterator<Item = char>> Lexer<Iter> {
         buffer
     }
 
+    // takes any char until another " is found
     fn take_literal(&mut self) -> Result<String, String> {
         let mut buffer = String::new();
-        
+
         loop {
             if let Some(character) = self.next() {
                 if character == '"' {
@@ -116,68 +131,216 @@ impl<Iter: Iterator<Item = char>> Lexer<Iter> {
         }
     }
 
+    // matches the current char to a token and pushes it to the output
     fn tokenize(&mut self) {
+        self.offset = self.cursor;
+
         match self.ch {
-            '(' => self.push(Token::LParen { offset: self.cursor }),
-            ')' => self.push(Token::RParen { offset: self.cursor }),
-            ';' => self.push(Token::SemiColon { offset: self.cursor }),
+            '(' => self.push(Token::LParen {
+                offset: self.offset,
+            }),
+            ')' => self.push(Token::RParen {
+                offset: self.offset,
+            }),
+            ';' => self.push(Token::SemiColon {
+                offset: self.offset,
+            }),
+            '#' => self.push(Token::Hash {
+                offset: self.offset,
+            }),
+            '%' => self.push(Token::Modulo {
+                offset: self.offset,
+            }),
+            '*' => self.push(Token::Star {
+                offset: self.offset,
+            }),
+            ':' => self.push(Token::Colon {
+                offset: self.offset,
+            }),
             '+' => {
                 if let Some(c) = self.next() {
                     if c == '=' {
-                        self.push(Token::PlusEqual { offset: self.cursor - 1 })
+                        self.push(Token::PlusEqual {
+                            offset: self.offset,
+                        })
                     } else if c == '+' {
-                        self.push(Token::PlusPlus { offset: self.cursor - 1 })
+                        self.push(Token::PlusPlus {
+                            offset: self.offset,
+                        })
                     } else {
-                        self.push(Token::Plus { offset: self.cursor - 1 });
+                        self.push(Token::Plus {
+                            offset: self.offset,
+                        });
                         self.tokenize();
                     }
                 } else {
-                    self.push(Token::Plus { offset: self.cursor })
+                    self.push(Token::Plus {
+                        offset: self.offset,
+                    })
                 }
-            },
+            }
+            '-' => {
+                if let Some(c) = self.next() {
+                    if c == '=' {
+                        self.push(Token::MinusEqual {
+                            offset: self.offset,
+                        });
+                    } else if c == '-' {
+                        self.push(Token::MinusMinus {
+                            offset: self.offset,
+                        });
+                    } else if c == '>' {
+                        self.push(Token::Arrow {
+                            offset: self.offset,
+                        });
+                    } else {
+                        self.push(Token::Minus {
+                            offset: self.offset,
+                        });
+                        self.tokenize();
+                    }
+                } else {
+                    self.push(Token::Minus {
+                        offset: self.offset,
+                    });
+                }
+            }
+            '/' => {
+                if let Some(c) = self.next() {
+                    if c == '/' {
+                        self.push(Token::SlashSlash {
+                            offset: self.offset,
+                        });
+                    } else {
+                        self.push(Token::Slash {
+                            offset: self.offset,
+                        });
+                        self.tokenize();
+                    }
+                } else {
+                    self.push(Token::Slash {
+                        offset: self.offset,
+                    });
+                }
+            }
             '=' => {
                 if let Some(c) = self.next() {
                     if c == '=' {
-                        self.push(Token::EqualEqual { offset: self.cursor - 1 })
+                        self.push(Token::EqualEqual {
+                            offset: self.offset,
+                        });
                     } else {
-                        self.push(Token::Equal { offset: self.cursor - 1 });
+                        self.push(Token::Equal {
+                            offset: self.offset,
+                        });
                         self.tokenize();
                     }
                 } else {
-                    self.push(Token::Equal { offset: self.cursor })
+                    self.push(Token::Equal {
+                        offset: self.offset,
+                    });
                 }
-            },
+            }
+            '<' => {
+                if let Some(c) = self.next() {
+                    if c == '=' {
+                        self.push(Token::LessEqual {
+                            offset: self.offset,
+                        });
+                    } else {
+                        self.push(Token::Less {
+                            offset: self.offset,
+                        });
+                        self.tokenize();
+                    }
+                } else {
+                    self.push(Token::Less {
+                        offset: self.offset,
+                    });
+                }
+            }
+            '!' => {
+                if let Some(c) = self.next() {
+                    if c == '=' {
+                        self.push(Token::BangEqual {
+                            offset: self.offset,
+                        });
+                    } else {
+                        self.push(Token::Bang {
+                            offset: self.offset,
+                        });
+                        self.tokenize();
+                    }
+                } else {
+                    self.push(Token::Bang {
+                        offset: self.offset,
+                    });
+                }
+            }
+            '>' => {
+                if let Some(c) = self.next() {
+                    if c == '=' {
+                        self.push(Token::MoreEqual {
+                            offset: self.offset,
+                        });
+                    } else if c == '>' {
+                        self.push(Token::DoubleArrow {
+                            offset: self.offset,
+                        });
+                    } else {
+                        self.push(Token::More {
+                            offset: self.offset,
+                        });
+                        self.tokenize();
+                    }
+                } else {
+                    self.push(Token::More {
+                        offset: self.offset,
+                    });
+                }
+            }
 
             // number literals
             '0'..='9' => {
                 let number = self.take_number();
-                self.push(Token::Number { offset: self.cursor, value: number })
+                self.push(Token::Number {
+                    offset: self.offset,
+                    value: number,
+                })
             }
 
             // string literal
-            '"' => {
-                match self.take_literal() {
-                    Ok(literal) => self.push(Token::String { offset: self.cursor, value: literal }),
-                    Err(_) => panic!("[DY4] Non terminating literal!"),
-                }
-            }
+            '"' => match self.take_literal() {
+                Ok(literal) => self.push(Token::String {
+                    offset: self.offset,
+                    value: literal,
+                }),
+                Err(_) => panic!("[DY4] Non terminating literal!"),
+            },
 
             // identifers & keywords
             'a'..='z' | 'A'..='Z' => {
                 let alphanum = self.take_alphanum();
-                
+
                 // match keywords
                 match alphanum.as_str() {
-                    "return" => self.push(Token::Return { offset: self.cursor }),
-                    "fn" => self.push(Token::Fn { offset: self.cursor }),
+                    "return" => self.push(Token::Return {
+                        offset: self.offset,
+                    }),
+                    "fn" => self.push(Token::Fn {
+                        offset: self.offset,
+                    }),
 
                     // identifers
-                    _ => self.push(Token::Indent { offset: self.cursor, value: alphanum }),
+                    _ => self.push(Token::Indent {
+                        offset: self.offset,
+                        value: alphanum,
+                    }),
                 }
 
                 self.tokenize();
             }
-            _ => {},
+            _ => {}
         }
     }
 }
--- src/token.rs
+++ src/token.rs
@@ -3,15 +3,37 @@ pub enum Token {
     // grouping
     LParen { offset: usize },
     RParen { offset: usize },
+    LBrac { offset: usize },
+    RBrac { offset: usize },
+    LCurl { offset: usize },
+    RCurl { offset: usize },
 
     // operators
     Plus { offset: usize },
     PlusPlus { offset: usize },
     PlusEqual { offset: usize },
+    Minus { offset: usize },
+    MinusMinus { offset: usize },
+    MinusEqual { offset: usize },
+    Slash { offset: usize },
+    SlashSlash { offset: usize },
+    Modulo { offset: usize },
+    Star { offset: usize },
+    Hash { offset: usize },
+    SemiColon { offset: usize },
+    Arrow { offset: usize },
+    DoubleArrow { offset: usize },
+    Colon { offset: usize },
 
     // comparisons
     Equal { offset: usize },
     EqualEqual { offset: usize },
+    Less { offset: usize },
+    LessEqual { offset: usize },
+    More { offset: usize },
+    MoreEqual { offset: usize },
+    Bang { offset: usize },
+    BangEqual { offset: usize },
 
     // literals
     String { offset: usize, value: String },
@@ -21,8 +43,10 @@ pub enum Token {
     // keywords
     Return { offset: usize },
     Fn { offset: usize },
+    If { offset: usize },
+    Else { offset: usize },
+    Elif { offset: usize },
 
     // other
-    SemiColon { offset: usize },
     Eof { offset: usize },
 }
